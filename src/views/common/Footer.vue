5109d01cb0482ef07787fa72c8c3efc57ec8a91f
aaaisan <aaaisan@gmail.com>
2021-03-11 10:45:01 +0800

    first commit

---
+++ src/views/common/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+  <footer class="footer">
+    <div class="columns is-mobile is-centered">
+      <div class="field is-grouped is-grouped-multiline">
+        <div class="control">
+          <div class="tags has-addons">
+            <span class="tag is-dark">
+              Copy &copy; &nbsp;
+<!--              <span class="icon">-->
+<!--                <i class="fa fa-github"></i>-->
+<!--              </span>-->
+            </span>
+            <a
+              class="tag is-light"
+              href="https://since70.com"
+            >Since70</a>
+<!--            <span class="tag is-light">-->
+<!--              Copy &copy; &nbsp;-->
+<!--              <span class="icon">-->
+<!--                <i class="fa fa-github"></i>-->
+<!--              </span>-->
+<!--            </span>-->
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: {},
+  watch: {},
+  data: function () {
+    return {
+      content: ""
+    };
+  },
+  components: {},
+  methods: {}
+};
+</script>
